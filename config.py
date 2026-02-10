80a2656259ecc4f3d4df829e7ce6b41a71dec21a
Aidan <samuraishibe1@gmail.com>
2026-02-09 20:06:29 -0600

    initial commit, config + loaded obsidian vault

---
+++ config.py
@@ -0,0 +1,19 @@
+"""Configuration for Obsidian RAG project."""
+from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Obsidian vault path
+VAULT_PATH = Path(
+    "/Users/aidanwu/Library/CloudStorage/GoogleDrive-samuraishibe1@gmail.com/My Drive/Aidan's Vault"
+)
+
+# Persistence directory for the vector store index
+PERSIST_DIR = Path(".obsrag/index")
+
+# Number of top results to retrieve
+TOP_K = 5
+
+# Embedding model to use (OpenAI)
+EMBEDDING_MODEL = "text-embedding-3-small"
